22bd81d8fb90dbccc99bbdcad814bb2a883a264e
bronze <bronze@qq.com>
2020-09-01 19:36:19 +0800

    Do Git Commit From IDEA!~

---
+++ RedisJedis/src/test/java/com/atguigu/JedisTest.java
@@ -0,0 +1,42 @@
+package com.atguigu;
+
+import org.junit.Test;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.Protocol;
+
+import java.util.List;
+
+public class JedisTest {
+    @Test
+    //jedis连接Redis
+    public void test1(){
+        Jedis jedis = new Jedis("192.168.1.102", 6379);
+        String ping = jedis.ping();
+        System.out.println("ping="+ping);
+        Long l = jedis.lpush("Color", "Red", "Blue", "Yellow", "Green", "Black", "White", "Grey");
+        System.out.println(l);
+        List<String> res = jedis.lrange("Color", 0, -1);
+        for (String re : res) {
+            System.out.print(re+"    ");
+        }
+        jedis.close();
+    }
+    @Test
+    public void test2(){
+        //JedisPool
+        String host = "192.168.1.102";
+        int port = Protocol.DEFAULT_PORT;
+        //创建连接池对象
+        JedisPool jp = new JedisPool(host, port);
+        //获取Jedis对象连接Redis
+        Jedis res = jp.getResource();
+        String ping = res.ping();
+        Long abc = res.lpush("Character", "A", "C", "E", "F");
+        List<String> character = res.lrange("Character", 0, -1);
+        for (String s : character) {
+            System.out.println(s);
+        }
+        jp.close();
+    }
+}
